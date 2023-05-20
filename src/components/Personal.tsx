7819812032d0f05573f967deb8563c5a05485334
CarlXD123 <64506247+CarlXD123@users.noreply.github.com>
2023-05-20 15:54:52 -0500

    Update Personal.tsx

--- src/components/Personal.tsx
+++ src/components/Personal.tsx
@@ -114,9 +114,7 @@ class Personal extends React.Component<{ navigate: NavigateFunction }, any>{
                     <br></br>
                     <br></br>
                     <br></br>
-                    <div>
-                        <TbPersonal  texto={this.state.buscarTextoClone} opcion={this.state.buscarSeleccionarClone} />
-                    </div>
+                  
 
                 </Contenido>
             </div>
