5fd2e2378981f95e4818252533f0e14a8c1db6b2
CarlXD123 <64506247+CarlXD123@users.noreply.github.com>
2023-05-21 11:51:00 -0500

    Update tbPersonal.tsx

--- src/components/tablas/tbPersonal.tsx
+++ src/components/tablas/tbPersonal.tsx
@@ -612,7 +612,7 @@ export default function TbPersonal({ texto, opcion }: any) {
           <Box sx={style}>
             <InputLabel style={{ color: "black", fontFamily: "Quicksand", fontWeight: "500", fontSize: "1.5rem" }} >Detalles del empleado</InputLabel >
             <TabContext value={values} >
-             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+             <Box>
                 <Tabs value={values} scrollButtons="auto" indicatorColor="primary" textColor="primary" onChange={handleChangeValor}  >
                   <Tab className="h-64 normal-case" label="Datos personales" value="1" />
                   <Tab className="h-64 normal-case" label="Domicilio" value="2" />
