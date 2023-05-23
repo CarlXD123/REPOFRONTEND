8ede5e2c8ff8d9593813235de346a0815f69869d
CarlXD123 <64506247+CarlXD123@users.noreply.github.com>
2023-05-23 14:43:45 -0500

    Update tbRegPersonal.tsx

--- src/components/personal/registrar/tbRegPersonal.tsx
+++ src/components/personal/registrar/tbRegPersonal.tsx
@@ -930,6 +930,8 @@ export default function TbRegPersonal() {
                 </Grid>
                 <br></br>
                 <div>
+                    <br></br>
+                    <br></br>
                     <CardContent style={{ backgroundColor: "white", borderRadius: "12px" }}>
                         <div>
                             <TabContext value={values}>
