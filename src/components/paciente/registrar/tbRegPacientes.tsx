73fbfe1af571f97cf43b9ed05cf7d294279eb27e
CarlXD123 <64506247+CarlXD123@users.noreply.github.com>
2023-05-23 14:54:45 -0500

    Update tbRegPacientes.tsx

--- src/components/paciente/registrar/tbRegPacientes.tsx
+++ src/components/paciente/registrar/tbRegPacientes.tsx
@@ -776,6 +776,8 @@ export default function TbRegPaciente() {
                 </Grid>
                 <br></br>
                 <div>
+                    <br></br>
+                    <br></br>
                     <CardContent style={{ backgroundColor: "white", borderRadius: "12px" }}>
                         <div>
                             <TabContext value={values}>
